65ca7b128021cc857977994b68e57b92be127c07
adrianavinicki <adriana.vinicki@gmail.com>
2023-05-20 13:03:55 -0300

    cambia nombre base detail x sgda vez

--- src/models/Detailorder.js
+++ src/models/Detailorder.js
@@ -2,9 +2,9 @@ const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo HOLLA
+  // defino el modelo probando
   sequelize.define(
-    "orderdetail",
+    "detailorder",
     {
       detail_id: {
         type: DataTypes.INTEGER,
