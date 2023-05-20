b3a0289e8f432f19a3c3256f624427ecfc1eb72d
adrianavinicki <adriana.vinicki@gmail.com>
2023-05-20 12:42:40 -0300

    cambia nombre base detail y genera relaciones de modelos

--- src/db.js
+++ src/db.js
@@ -37,56 +37,36 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product, OrderDetail, Order, Payment, Rating, Stock, User } =
+const { Product, Orderdetail, Order, Payment, Rating, Stock, User } =
   sequelize.models;
 
 // Relacion productos/rating
 Product.hasMany(Rating);
 Rating.belongsTo(Product);
-//relacion entre User y User_travel
-/*User.belongsToMany(User_travel, { through: "travel_user" });
-User_travel.belongsToMany(User, { through: "travel_user" });
-//relacion entre Bills y User_travel
-Bills.belongsToMany(User_travel, { through: "bills_travels" });
-User_travel.belongsToMany(Bills, { through: "bills_travels" });*/
+//relacion entre user/rating
+User.hasMany(Rating, { as: "ratings", foreignKey: "user_id" });
+Rating.belongsTo(User, { foreignKey: "user_id" });
+// relacion user/productos favoritos
+User.belongsToMany(Product, { through: "favoritos" });
+Product.belongsToMany(User, { through: "favoritos" });
+// relacion producto/stock
+Product.belongsTo(Stock);
+Stock.belongsTo(Product, { foreignKey: "id_product" });
+// relacion payment/order
+Payment.hasOne(Order);
+Order.hasOne(Payment);
+//relacion producto/orderdetail
+Product.hasMany(Orderdetail);
+Orderdetail.belongsTo(Product);
+//relacion orders/orderdetail
+Order.hasMany(Orderdetail);
+Orderdetail.belongsTo(Order);
 //relacion entre Order_Detail y User
-//User.hasMany(OrderDetail, { as: "order_detail", foreignKey: "user_id" });
-//OrderDetail.belongsTo(User, { through: "user_id" });
-/*//relacion entre Services y Room
-Services.belongsToMany(Room, { through: "room_services" });
-Room.belongsToMany(Services, { through: "room_services" });
-//relacion entre Hotel y Room
-Hotel.hasMany(Room, { as: "rooms", foreignKey: "idHotels" });
-Room.belongsTo(Hotel, { foreignKey: "idHotels" });
-//relacion entre User y Room
-User.belongsToMany(Room, { through: "user_room" });
-Room.belongsToMany(User, { through: "user_room" });
-//relacion entre Room y Amenities
-Amenities.belongsToMany(Room, { through: "amenities_room" });
-Room.belongsToMany(Amenities, { through: "amenities_room" });
-//relacion entre User y Partners
-User.hasMany(Partners);
-Partners.belongsTo(User);
-//relacion entre Hotel y About_us
-Hotel.hasOne(About_us);
-About_us.belongsTo(Hotel);
-
-//! Relacion cat_inventory(inventory), cat_room_type(room_type) y cat_hotel_info(hotel_info) ----------------
-Cat_hotel_info.hasMany(Cat_room_inventory, { foreignKey: "hotel_id" });
-Cat_room_inventory.belongsTo(Cat_hotel_info, { foreignKey: "hotel_id" });
-Cat_room_inventory.belongsTo(Cat_room_type, { foreignKey: "cat_room_type_id" });
-Cat_room_type.hasMany(Cat_room_inventory, { foreignKey: "cat_room_type_id" });
-//!----------------
-
-// En el modelo Review
-Reviews.belongsTo(User, { foreignKey: "idUser" });
-Reviews.belongsTo(Hotel, { foreignKey: "hotelId" });
-
-// En el modelo User
-User.hasMany(Reviews, { foreignKey: "idUser" });
-
-// En el modelo Hotel
-Hotel.hasMany(Reviews, { foreignKey: "hotelId" });*/
+User.hasMany(Orderdetail);
+Orderdetail.belongsTo(User);
+// relacion usuarios/pagos
+User.hasMany(Payment);
+Payment.belongsTo(User);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
