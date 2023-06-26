d398f29003490d5fc821c52e13fd579b02120e9a
Martin Fuks <martinfukss@gmail.com>
2023-06-26 20:37:01 -0300

    no subir

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ require("dotenv").config();
 const { PORT } = process.env;
 //Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
-  //conn.sync({ force: false }).then(async () => {
+//conn.sync({ force: false }).then(async () => {
   //lo dejamos en true mientras construimos las bases y cargamos, luego va a false para que no borre lo cargado
   server.listen(PORT, async () => {
-    loadProductsInDB(); // eslint-disable-line no-console
+    loadProductsInDB(); // eslint-disable-line no-console, comentar despues de cargar los productos.
     console.log(`listening at ${PORT}`);
   });
 });
